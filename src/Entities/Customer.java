ce359f36a939386294930e7f5b6e2fc6b2b2ffc7
zhannyfocs-sketch <zhannyfocs@gmail.com>
2026-01-13 13:12:07 +0500

    Zhannel Malikaidar -- Customer entity setter+getter

--- src/Entities/Customer.java
+++ src/Entities/Customer.java
@@ -2,13 +2,57 @@ package Entities;
 
 public class Customer {
     private int id;
-    private String fullname;
+    private String fullName;
     private String email;
-    private String driverlicenseid;
+    private String driverLicenseId;
 
-    public Customer(String fullname, String email, String driverlicenseid) {
-        this.fullname = fullname;
+    public Customer(String fullName, String email, String driverLicenseId) {
+        setFullName(fullName);
+        setEmail(email);
+        setDriverLicenseId(driverLicenseId);
+    }
+    public Customer(int id, String fullName, String email, String driverLicenseId) {
+        this.id = id;
+        setFullName(fullName);
+        setEmail(email);
+        setDriverLicenseId(driverLicenseId);
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        if (fullName == null) {
+            throw  new IllegalArgumentException("fullName cannot be empty");
+        }
+        this.fullName = fullName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        if (email == null) {
+            throw  new IllegalArgumentException("email cannot be empty");
+        }
         this.email = email;
-        this.driverlicenseid = driverlicenseid;
+    }
+    public String getDriverLicenseId() {
+        return driverLicenseId;
+    }
+    public void setDriverLicenseId(String driverLicenseId) {
+        if (driverLicenseId == null) {
+            throw  new IllegalArgumentException("driverLicenseId cannot be empty");
+        }
+        this.driverLicenseId = driverLicenseId;
+    }
+
+    @Override
+    public String toString() {
+        return "id"+id+"fullName"+fullName+"email"+email+"driverLicenseId"+driverLicenseId;
     }
 }
